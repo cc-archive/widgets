ea5faaaf1c0b4f5d87c71ee70f5df6d35c8260cf
nyergler <nyergler@committer.creativecommons.org>
2007-02-13 22:16:33 +0000

    Importing widgets module.

---
+++ ccmixter_editors_picks.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,31 @@
+var localizedStrings = new Object;
+
+// Button strings
+localizedStrings["Done"] = "Done";
+localizedStrings["Subscribe to Podcast"] = "Subscribe to Podcast";
+
+// Days of the week
+localizedStrings["Sunday"] = "Sunday";
+localizedStrings["Monday"] = "Monday";
+localizedStrings["Tuesday"] = "Tuesday";
+localizedStrings["Wednesday"] = "Wednesday";
+localizedStrings["Thursday"] = "Thursday";
+localizedStrings["Friday"] = "Friday";
+localizedStrings["Saturday"] = "Saturday";
+
+// Months of the year
+localizedStrings["January"] = "January";
+localizedStrings["February"] = "February";
+localizedStrings["March"] = "March";
+localizedStrings["April"] = "April";
+localizedStrings["May"] = "May";
+localizedStrings["June"] = "June";
+localizedStrings["July"] = "July";
+localizedStrings["August"] = "August";
+localizedStrings["September"] = "September";
+localizedStrings["October"] = "October";
+localizedStrings["November"] = "November";
+localizedStrings["December"] = "December";
+localizedStrings['My Podcast'] = unescape('ccmixter.org editors%27 picks');
+localizedStrings[unescape('ccmixter.org editor%27s picks')] = unescape('ccmixter.org editors%27 picks');
+localizedStrings['trackbutton_label'] = unescape('%3E');
